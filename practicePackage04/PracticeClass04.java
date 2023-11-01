81543deda5ec26412cf6a804df8cbd906926aaa4
Shilpa <shilpa25988@gmail.com>
2023-11-01 22:53:20 +0000

    filemodified

--- practicePackage04/PracticeClass04.java
+++ practicePackage04/PracticeClass04.java
@@ -4,6 +4,7 @@ public class PracticeClass04 {
     public static void main(String[] args) {
         System.out.println("What is SDLC?");
         System.out.println("What is STLC?");
+        System.out.println("how many stages in STLC?");
     }
 
 }
